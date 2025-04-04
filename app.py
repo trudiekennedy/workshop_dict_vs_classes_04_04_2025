72ca6bd2825b9c5812fa9364366039bef26aec74
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2025-04-04 09:45:04 +0100

    Delete solution

--- app.py
+++ app.py
@@ -20,13 +20,5 @@ persons = [
 # Make a persons list with at least 2 dictionaries representing a person with hobbies - DONE
 
 # create a function to print out the name of the persons
-for person in persons:
-    print(person["name"])
 
 # create a function to print out all hobbies of the persons eg. print(all_hobbies()) => ['tennis', 'working out', 'maths']
-hobbies = []
-for person in persons:
-    for hobby in person["hobbies"]:
-        hobbies.append(hobby)
-
-print(hobbies)
